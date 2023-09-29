fc1b6332760fdae437847b34490fcfc68fab503d
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-29 10:36:06 -0300

    Checkpoint 1

---
+++ script.js
@@ -0,0 +1,25 @@
+let nome = 'Rafael';
+let idade = 21;
+let status = true;
+let valorProduto = 10.50;
+let listaTimes = ['Borussia Dortmund', 'FC Barcelona', 'Manchester City'];
+
+console.log(nome);
+console.log(idade);
+console.log(status);
+console.log(valorProduto);
+console.log(listaTimes);
+
+let var1 = 2;
+let var2 = 1;
+
+let sum = var1 + var2;
+let sub = var1 - var2;
+let mult = var1 * var2;
+let divi = var1 / var2;
+
+console.log(sum);
+console.log(sub);
+console.log(mult);
+console.log(divi);
+
